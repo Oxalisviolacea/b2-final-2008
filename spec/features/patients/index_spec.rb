78496a6b4569907a60b989f1aee0cf8b207ce9f2
Hanna Davis <hanna.e.davis@gmail.com>
2020-11-09 11:19:08 -0700

    Change test to test order oldest to youngest

--- spec/features/patients/index_spec.rb
+++ spec/features/patients/index_spec.rb
@@ -3,18 +3,20 @@ require 'rails_helper'
 RSpec.describe 'As a visitor', type: :feature do
   describe "When I visit the patient's index page" do
     before(:each) do
-      @hospital_1 = Hospital.create!(name: 'Denver Health')
+      @hospital_1 = Hospital.create!(name: 'Grey Sloan Memorial Hospital')
       @doctor_1 = Doctor.create!(name: 'Alex', specialty: 'Emergency Medicine', university: 'Seattle State', hospital_id: @hospital_1.id)
       @doctor_2 = Doctor.create!(name: 'Hanna', specialty: 'Pediatrics', university: 'Western Colorado State', hospital_id: @hospital_1.id)
-      @patient_1 = @doctor_1.patients.create!(name: 'Tim', age: 50)
-      @patient_2 = @doctor_1.patients.create!(name: 'Brian', age: 60)
-      @patient_3 = @doctor_2.patients.create!(name: 'Meg', age: 60)
+      @patient_1 = @doctor_1.patients.create!(name: 'Katie Bryce', age: 24)
+      @patient_2 = @doctor_1.patients.create!(name: 'Denny Duquette', age: 39)
+      @patient_3 = @doctor_2.patients.create!(name: 'Rebecca Pope', age: 32)
+      @patient_4 = @doctor_2.patients.create!(name: 'Zola Shepherd', age: 2)
     end
 
-    it 'I can see all of the patients listed oldes to youngest' do
+    it 'I can see all of the patients listed oldest to youngest' do
       visit "/patients"
       expect("#{@patient_2.name}").to appear_before("#{@patient_3.name}")
       expect("#{@patient_3.name}").to appear_before("#{@patient_1.name}")
+      expect("#{@patient_1.name}").to appear_before("#{@patient_4.name}")
     end
   end
 end
